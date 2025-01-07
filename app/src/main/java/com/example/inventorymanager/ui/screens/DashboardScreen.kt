dbfdc00e03f90fe4f35ccd0cff8dcb5d8401683d
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2025-01-07 11:28:42 -0500

    Fix the problems with commits

--- app/src/main/java/com/example/inventorymanager/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/inventorymanager/ui/screens/DashboardScreen.kt
@@ -3,7 +3,8 @@ package com.example.inventorymanager.ui.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
@@ -16,34 +17,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.example.inventorymanager.ui.theme.InventoryManagerTheme
-import com.example.inventorymanager.data.repository.MockRepository
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(navController: NavController, viewModel: DashboardViewModel) {
     val products by viewModel.products.observeAsState(emptyList())
-    var searchQuery by remember { mutableStateOf("") }
-    var categoryFilter by remember { mutableStateOf("") }
-    var minPriceFilter by remember { mutableStateOf("") }
-    var maxPriceFilter by remember { mutableStateOf("") }
-
-    // Filtrar productos basados en búsqueda y filtros
-    val filteredProducts = products.orEmpty().filter { product ->
-        (searchQuery.isEmpty() || product.nombre.contains(searchQuery, ignoreCase = true)) &&
-                (categoryFilter.isEmpty() || product.categoria_nombre?.contains(categoryFilter, ignoreCase = true) == true) &&
-                (minPriceFilter.isBlank() || product.precio >= minPriceFilter.toDoubleOrNull() ?: Double.MIN_VALUE) &&
-                (maxPriceFilter.isBlank() || product.precio <= maxPriceFilter.toDoubleOrNull() ?: Double.MAX_VALUE)
-    }
 
     LaunchedEffect(Unit) {
         viewModel.fetchProducts()
@@ -63,58 +43,8 @@ fun DashboardScreen(navController: NavController, viewModel: DashboardViewModel)
             modifier = Modifier.padding(vertical = 16.dp)
         )
 
-        // Campos de búsqueda avanzada
-        OutlinedTextField(
-            value = searchQuery,
-            onValueChange = { searchQuery = it },
-            label = { Text("Buscar por nombre", color = MaterialTheme.colorScheme.primary) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = MaterialTheme.colorScheme.primary,
-                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
-            )
-        )
-        OutlinedTextField(
-            value = categoryFilter,
-            onValueChange = { categoryFilter = it },
-            label = { Text("Categoría", color = MaterialTheme.colorScheme.primary) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = MaterialTheme.colorScheme.primary,
-                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
-            )
-        )
-        OutlinedTextField(
-            value = minPriceFilter,
-            onValueChange = { minPriceFilter = it },
-            label = { Text("Precio mínimo", color = MaterialTheme.colorScheme.primary) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = MaterialTheme.colorScheme.primary,
-                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
-            )
-        )
-        OutlinedTextField(
-            value = maxPriceFilter,
-            onValueChange = { maxPriceFilter = it },
-            label = { Text("Precio máximo", color = MaterialTheme.colorScheme.primary) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = MaterialTheme.colorScheme.primary,
-                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
-            )
-        )
-
         LazyColumn {
-            filteredProducts?.groupBy { it.categoria_nombre }?.forEach { (categoriaNombre, productos) ->
+            products.groupBy { it.categoria_nombre }.forEach { (categoriaNombre, productos) ->
                 item {
                     Text(
                         text = categoriaNombre ?: "Sin Categoría",
@@ -134,47 +64,29 @@ fun DashboardScreen(navController: NavController, viewModel: DashboardViewModel)
                                 // Navega a la pantalla de detalles del producto
                                 navController.navigate("product_detail/${product.id}")
                             },
-                        colors = CardDefaults.cardColors(
-                            containerColor = MaterialTheme.colorScheme.surface,
-                            contentColor = MaterialTheme.colorScheme.onSurface
-                        ),
                         elevation = CardDefaults.cardElevation(4.dp)
                     ) {
                         Column(modifier = Modifier.padding(16.dp)) {
                             Text(
                                 text = product.nombre,
                                 style = MaterialTheme.typography.bodyLarge.copy(
-                                    fontWeight = FontWeight.Bold,
-                                    color = MaterialTheme.colorScheme.primary
+                                    fontWeight = FontWeight.Bold
                                 )
                             )
                             Text(
                                 text = "Precio: ${product.precio} | Stock: ${product.stock}",
-                                style = MaterialTheme.typography.bodyMedium.copy(
-                                    color = MaterialTheme.colorScheme.onSurface
-                                )
+                                style = MaterialTheme.typography.bodyMedium
                             )
                             Text(
                                 text = "Descripción: ${product.descripcion}",
-                                style = MaterialTheme.typography.bodySmall.copy(
-                                    color = MaterialTheme.colorScheme.onSurface
-                                )
+                                style = MaterialTheme.typography.bodySmall
                             )
                         }
                     }
                 }
             }
         }
+
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DashboardScreenPreview() {
-    val navController = rememberNavController()
-    val mockRepository = MockRepository()
-    val viewModel = DashboardViewModel(repository = mockRepository)
-    InventoryManagerTheme {
-        DashboardScreen(navController = navController, viewModel = viewModel)
-    }
-}
