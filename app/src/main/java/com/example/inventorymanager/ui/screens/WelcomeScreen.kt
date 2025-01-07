8f239ce20e8563146986ce0505d53b6dad19b823
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2025-01-07 11:14:46 -0500

    Revert "Add new icon"
    This reverts commit b70e464c8c1c8f6252a059d382c8ed337a94d8eb.

--- app/src/main/java/com/example/inventorymanager/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/inventorymanager/ui/screens/WelcomeScreen.kt
@@ -1,20 +1,17 @@
 package com.example.inventorymanager.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.inventorymanager.R
 import com.example.inventorymanager.ui.navigation.Screen
-import com.example.inventorymanager.ui.theme.InventoryManagerTheme
 
 @Composable
 fun WelcomeScreen(navController: NavController) {
@@ -25,26 +22,13 @@ fun WelcomeScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.inventory), //Aqui va un icono
-            contentDescription = "Inventory Logo",
-            modifier = Modifier.size(128.dp)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = "Bienvenido a Inventory Manager",
-            style = MaterialTheme.typography.titleLarge.copy(
-                color = MaterialTheme.colorScheme.primary,
-                fontSize = 24.sp
-            )
+            style = MaterialTheme.typography.titleLarge
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = { navController.navigate(Screen.Dashboard.route) },
-            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "Ir al Dashboard", color = MaterialTheme.colorScheme.onSecondary)
+        Button(onClick = { navController.navigate(Screen.Dashboard.route) }) {
+            Text(text = "Ir al Dashboard")
         }
     }
 }
@@ -52,7 +36,5 @@ fun WelcomeScreen(navController: NavController) {
 @Preview(showBackground = true)
 @Composable
 fun WelcomeScreenPreview() {
-    InventoryManagerTheme {
-        WelcomeScreen(navController = rememberNavController())
-    }
+    WelcomeScreen(navController = rememberNavController())
 }
