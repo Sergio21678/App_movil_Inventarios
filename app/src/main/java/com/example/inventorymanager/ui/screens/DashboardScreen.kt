10cbb938dd27cc5891e3252603bf79bbb324061d
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2025-01-10 17:02:55 -0500

    Add new interface with geometric figures

--- app/src/main/java/com/example/inventorymanager/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/example/inventorymanager/ui/screens/DashboardScreen.kt
@@ -1,5 +1,6 @@
 package com.example.inventorymanager.ui.screens
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -9,7 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,67 +30,100 @@ fun DashboardScreen(navController: NavController, viewModel: DashboardViewModel 
         viewModel.fetchProducts()
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "Lista de Productos",
-            style = MaterialTheme.typography.headlineSmall.copy(
-                color = MaterialTheme.colorScheme.primary,
-                fontWeight = FontWeight.Bold
-            ),
-            modifier = Modifier.padding(vertical = 16.dp)
-        )
+    Box(modifier = Modifier.fillMaxSize()) {
+        DashboardBackgroundShapes() // Add the background shapes
 
-        LazyColumn {
-            products?.groupBy { it.categoria_nombre }?.forEach { (categoriaNombre, productos) ->
-                item {
-                    Text(
-                        text = categoriaNombre ?: "Sin Categoría",
-                        style = MaterialTheme.typography.headlineSmall.copy(
-                            fontWeight = FontWeight.Bold,
-                            color = MaterialTheme.colorScheme.primary
-                        ),
-                        modifier = Modifier.padding(vertical = 8.dp)
-                    )
-                }
-                items(productos) { product ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 4.dp)
-                            .clickable {
-                                // Navega a la pantalla de detalles del producto
-                                navController.navigate("product_detail/${product.id}")
-                            },
-                        elevation = CardDefaults.cardElevation(4.dp),
-                        colors = CardDefaults.cardColors(
-                            containerColor = MaterialTheme.colorScheme.surface,
-                            contentColor = MaterialTheme.colorScheme.onSurface
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Lista de Productos",
+                style = MaterialTheme.typography.headlineSmall.copy(
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+
+            LazyColumn {
+                products?.groupBy { it.categoria_nombre }?.forEach { (categoriaNombre, productos) ->
+                    item {
+                        Text(
+                            text = categoriaNombre ?: "Sin Categoría",
+                            style = MaterialTheme.typography.headlineSmall.copy(
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.primary
+                            ),
+                            modifier = Modifier.padding(vertical = 8.dp)
                         )
-                    ) {
-                        Column(modifier = Modifier.padding(16.dp)) {
-                            Text(
-                                text = product.nombre,
-                                style = MaterialTheme.typography.bodyLarge.copy(
-                                    fontWeight = FontWeight.Bold
+                    }
+                    items(productos) { product ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
+                                .clickable {
+                                    // Navega a la pantalla de detalles del producto
+                                    navController.navigate("product_detail/${product.id}")
+                                },
+                            elevation = CardDefaults.cardElevation(4.dp),
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.surface,
+                                contentColor = MaterialTheme.colorScheme.onSurface
+                            )
+                        ) {
+                            Column(modifier = Modifier.padding(16.dp)) {
+                                Text(
+                                    text = product.nombre,
+                                    style = MaterialTheme.typography.bodyLarge.copy(
+                                        fontWeight = FontWeight.Bold
+                                    )
                                 )
-                            )
-                            Text(
-                                text = "Precio: ${product.precio} | Stock: ${product.stock}",
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                            Text(
-                                text = "Descripción: ${product.descripcion}",
-                                style = MaterialTheme.typography.bodySmall
-                            )
+                                Text(
+                                    text = "Precio: ${product.precio} | Stock: ${product.stock}",
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                                Text(
+                                    text = "Descripción: ${product.descripcion}",
+                                    style = MaterialTheme.typography.bodySmall
+                                )
+                            }
                         }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun DashboardBackgroundShapes(modifier: Modifier = Modifier) {
+    val primaryColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f)
+    val secondaryColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.2f)
+
+    Canvas(modifier = modifier.fillMaxSize()) {
+        // Dibuja un cuadrado grande en la parte inferior derecha con transparencia
+        drawRect(
+            color = secondaryColor,
+            topLeft = Offset(x = size.width * 0.2f, y = size.height * 0.8f),
+            size = androidx.compose.ui.geometry.Size(800f, 800f)
+        )
+
+        // Dibuja un círculo grande en la parte superior izquierda con transparencia
+        drawCircle(
+            color = primaryColor,
+            radius = 700f,
+            center = Offset(x = size.width * 0.3f, y = size.height * 0.2f)
+        )
+
+        // Dibuja un círculo adicional en el centro con transparencia
+        drawCircle(
+            color = primaryColor,
+            radius = 800f,
+            center = Offset(x = size.width * 0.5f, y = size.height * 0.5f)
+        )
     }
 }
 
